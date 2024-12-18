66c557858e0e018960b5f71dcc0bc7853b7ac3ed
dimasdaffa <dimas.de2004@gmail.com>
2024-12-18 16:26:07 +0700

    yolov8 roboflow+tesseractOCR

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("D:\\license-plate-yolov\\runs\\detect\\train16\\weights\\best.pt")
 
 # Perform object detection on an image (escaped backslashes)
-results = model("D:\\license-plate-yolov\\test_images\\CK_AUS_200113012101_20240619101605314_X372Y611W24H24_Van_Mitsubishi_white_051_01_03752.jpg", save=True)
+results = model("D:\license-plate-yolov\\test_images\plat (1).jpg", save=True)
 
 # Show results
 results[0].show()
